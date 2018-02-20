d95e4c849ec0292d0769991d5fb1a049fc99d2b8
Urmikl18 <pavlo.shevchenko+kraken@sciflow.net>
2018-02-20 17:25:23 +0100

    Organize imports

--- src/main/java/ps/changeclassifier/ChangeAnalyzer.java
+++ src/main/java/ps/changeclassifier/ChangeAnalyzer.java
@@ -6,10 +6,7 @@ import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
-import java.util.TreeMap;
 import java.util.TreeSet;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 import org.languagetool.JLanguageTool;
 import org.languagetool.language.AmericanEnglish;
